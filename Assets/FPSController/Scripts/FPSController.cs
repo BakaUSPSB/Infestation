089ab1fd5b33994b3cbfdfb134c58e525aa6789d
BakaUSPSB <144304219+BakaUSPSB@users.noreply.github.com>
2024-12-05 12:59:01 -0500

    Added terrain + simple players no ACT

---
+++ Assets/FPSController/Scripts/FPSController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class FPSController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+    
+    
+    
+    public float speed = 10f;
+    public float jumpForce = 5f;
+
+
+    public Transform cameraTransform;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
+    }
+    private void FixedUpdate() 
+    {
+        Vector3 moveDirection = (cameraTransform.forward * movementY + cameraTransform.right * movementX).normalized;
+        moveDirection.y = 0;
+
+        rb.AddForce(moveDirection * speed, ForceMode.Force);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        }
+    }
+}
